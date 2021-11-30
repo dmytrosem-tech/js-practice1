20d9f89471516e8917a07016236bd3fa3c2cd6f2
Dmytro Semeruk <deemoon83@gmail.com>
2021-11-30 21:11:49 +0200

    tabs-home

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import "./slider"
+import modals from './modules/modals'
+import tabs from "./modules/tabs";
+window/addEventListener("DOMContentLoaded", () => {
+    modals();
+    tabs(".glazing_slider",".glazing_block", ".glazing_content", "active");
+})
