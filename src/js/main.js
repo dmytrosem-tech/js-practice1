9f913ebd5d537c3f861e43150cd03940347912c1
Dmytro Semeruk <deemoon83@gmail.com>
2021-12-02 13:54:06 +0200

    fin

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,18 @@ import "./slider";
 import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
-import changeModalState from './modules/changeModalState'
+import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
+import images from "./modules/images";
 
 window /
   addEventListener("DOMContentLoaded", () => {
     "use strict";
 
     let modalState = {};
-    changeModalState(modalState)
-    
+    let deadline = "2022-01-01";
+
+    changeModalState(modalState);
 
     modals();
     tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
@@ -20,6 +23,14 @@ window /
       ".decoration_content > div > div",
       "after_click"
     );
-    tabs(".balcon_icons", ".balcon_icons_img", ".big_img > img", "do_image_more", "inline-block")
+    tabs(
+      ".balcon_icons",
+      ".balcon_icons_img",
+      ".big_img > img",
+      "do_image_more",
+      "inline-block"
+    );
     forms(modalState);
+    timer(".container1", deadline);
+    images();
   });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -8,7 +8,8 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll("[data-modal]");
+      windows = document.querySelectorAll("[data-modal]"),
+      scroll = calcScroll();
 
     trigger.forEach((item) => {
       item.addEventListener("click", (e) => {
@@ -20,6 +21,8 @@ const modals = () => {
         });
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
+
+        document.body.style.marginRight = `${scroll}px`;
         // document.body.classList.add("modal-open")   ------ Вариант с использованием класса из бутстрапа
       });
     });
@@ -30,7 +33,7 @@ const modals = () => {
       });
       modal.style.display = "none";
       document.body.style.overflow = "";
-
+      document.body.style.marginRight = `0px`;
       // document.body.classList.remove("modal-open")  ------ Вариант с использованием класса из бутстрапа
     });
 
@@ -41,6 +44,7 @@ const modals = () => {
         });
         modal.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
         // document.body.classList.remove("modal-open")   ------ Вариант с использованием класса из бутстрапа
       }
     });
@@ -53,19 +57,39 @@ const modals = () => {
     }, time);
   }
 
+  function calcScroll() {
+    let div = document.createElement("div");
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflow = "scroll";
+    div.style.visibility = "hidden";
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
+
   bindModal(
     ".popup_engineer_btn",
     ".popup_engineer",
     ".popup_engineer .popup_close"
   );
   bindModal(".phone_link", ".popup", ".popup .popup_close");
-  //   showModalByTime(".popup", 60000);
+  showModalByTime(".popup", 60000);
   bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
   bindModal(
-    '.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false
+    ".popup_calc_button",
+    ".popup_calc_profile",
+    ".popup_calc_profile_close",
+    false
   );
   bindModal(
-    '.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false
+    ".popup_calc_profile_button",
+    ".popup_calc_end",
+    ".popup_calc_end_close",
+    false
   );
 };
 
